c56f1700445b11774e01375f526698ddb23e7f89
grdall <alex.thogar@gmail.com>
2019-06-20 17:11:21 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # python-simple-weather-fetch
+
+A simple python program that fetches data from [MET Weather API](https://api.met.no/weatherapi) and returns current temp, temp for rest of the day, and temp for next 24 hours.
+
+## Usage (Windows)
+1. [Install Python](https://www.python.org/downloads/)
+2. Make sure Python is defined on PATH
+3. Navigate to folder in CLI
+4. $python weather.py
+
+Optional: [Define coordinates](https://www.latlong.net) for costum location
+
+## Possible additions:
+- Might add more detailed info, max/min predicted temp, tomorrow instead fo next 24 etc
+- Option to add custom lat/lon (Getting coords from place name posibble though Google API, but is overkill)
---
+++ weather.py
@@ -0,0 +1,101 @@
+from urllib.request import urlopen
+import xml.etree.ElementTree as ET
+from datetime import datetime
+
+# Coordinates
+# Oslo
+lat = "59.91"
+lon = "10.75"
+
+# Fetch and parse
+print("Fetching data...")
+
+url = "https://api.met.no/weatherapi/probabilityforecast/1.1/?lat=" + lat + "&lon=" + lon
+response = urlopen(url).read()
+parsed = ET.fromstring(response)
+
+# Time generated
+generated = parsed.get("created")
+generated = generated.replace("T", ", ")
+generated = generated.replace("Z", " ")
+
+# Get current time
+currenttime = str(datetime.now().time())
+currenthours = int(currenttime[0:2])
+times = parsed.findall("product/time")
+
+# Get average temp now (earliest entry)
+print("Extracting data...")
+
+firstResp = times[0].findall("location/probability")
+averageNow = 0
+
+i = 0
+while i < len(firstResp):
+    averageNow += float(firstResp[i].get("value"))
+    i += 1
+
+nFirstResp = i
+
+# Get average temp for rest of day
+dayArray = []
+dayResp = times[0:4]
+restDayIndex = 0
+
+i = 0
+while i < len(dayResp):
+    # print("Checking: " + str(dayResp[i].get("from")))
+    dayTimeFrom = dayResp[i].get("from").split("T")
+
+    if(int(dayTimeFrom[1][0:2]) >= currenthours): # currenthours
+        restDayIndex += 1
+        tmpProbability = dayResp[i].findall("location/probability")
+        
+        j = 0
+        tmpValue = 0
+        while j < len(tmpProbability):
+            tmpValue += float(tmpProbability[j].get("value"))
+            j += 1
+        
+        dayArray.append(dayTimeFrom[1][0:5] + " - %.2f C" % float(tmpValue / j))
+    
+    i += 1
+
+# Get average temp for tomorrow
+tomorrowArray = []
+end = restDayIndex + 4
+tomorrowResp = times[restDayIndex:end]
+
+i = 0
+while i < len(tomorrowResp):
+    # print("Checking: " + str(tomorrowResp[i].get("from")))
+    tomorrowTimeFrom = tomorrowResp[i].get("from").split("T")
+    tmpProbability = tomorrowResp[i].findall("location/probability")
+    
+    j = 0
+    tmpValue = 0
+    while j < len(tmpProbability):
+        tmpValue += float(tmpProbability[j].get("value"))
+        j += 1
+    
+    tomorrowArray.append(tomorrowTimeFrom[1][0:5] + " - %.2f C" % float(tmpValue / j))
+    i += 1
+
+# Print
+print("Generated at " + generated)
+
+print("\nCurrently %.2f C" % (averageNow / nFirstResp))
+
+print("\nRest of day")
+
+i = 0
+while i < len(dayArray):
+    print(dayArray[i])
+    i += 1
+
+print("\nNext 24 hours")
+
+i = 0
+while i < len(tomorrowArray):
+    print(tomorrowArray[i])
+    i += 1
